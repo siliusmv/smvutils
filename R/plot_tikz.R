d953a9042fd088e8263930e2147ca4a869d08ad1
Silius M. Vandeskog <smvandeskog@nr.no>
2025-01-13 09:12:48 +0100

    Remove expand = c(0, 0) from the map-plot function

--- R/plot_tikz.R
+++ R/plot_tikz.R
@@ -75,7 +75,7 @@ smv_latex_friendly_map_plot = function(x) {
   east_labels = paste0(abs(east_ticks), "$^\\circ$", ifelse(sign(east_ticks) == 1, "E", "W"))
   north_labels = paste0(abs(north_ticks), "$^\\circ$", ifelse(sign(north_ticks) == 1, "N", "S"))
   x +
-    ggplot2::scale_x_continuous(breaks = east_ticks, labels = east_labels, expand = c(0, 0)) +
-    ggplot2::scale_y_continuous(breaks = north_ticks, labels = north_labels, expand = c(0, 0))
+    ggplot2::scale_x_continuous(breaks = east_ticks, labels = east_labels) +
+    ggplot2::scale_y_continuous(breaks = north_ticks, labels = north_labels)
 }
 
